54670b3c3ef9dc23544cd8f5d683f31b981322cd
pankaj ram pawar <pankajrampawar@pankajs-MacBook-Pro-2.local>
2023-11-15 02:42:24 +0530

    project frontend completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import dividerImageDesktop from './images/pattern-divider-desktop.svg';
+import dividerImageMobile from './images/pattern-divider-mobile.svg';
+import dice from './images/icon-dice.svg';
 
 function App() {
+
+  const[quote, setQuote] = React.useState('');
+  const[id, setId] = React.useState('');
+
+  // const response = await axios.get('https://api.adviceslip.com/advice');
+  // console.log(response.data);
+  
+  async function fetchData() {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice')
+      console.log(response);
+      setQuote(response.data.slip.advice);
+      setId(response.data.slip.id);
+    } catch (error) {
+      console.error("error: ", error);
+    }
+  }
+  fetchData();
+
+  const handleClick = ()=>{
+    fetchData();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">ADVICE #{id}</div>
+      <p className="quote">{quote}</p>
+      <img className="dividerMobile" src={dividerImageMobile}/>
+      <img className="dividerDesktop" src={dividerImageDesktop}/>
+      <div className="dice-container"  onClick={handleClick}>
+        <img className="dice" src={dice} alt='dice image'/>
+      </div>
     </div>
   );
 }
